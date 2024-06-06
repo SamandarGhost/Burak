4f5c7534faa1c7d50ffb343ae3306b757c7a54aa
Sam <kimjeyeon03@icloud.com>
2024-06-06 15:26:42 +0900

    feat: connect mongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,11 @@
 import dotenv from 'dotenv';  // ModuleJs import ... from .... ; ishlatiladi. // CommonJsda const .... = require("....."); ishlatiladi.
 dotenv.config();
 
+import mongoose from 'mongoose';
 
+mongoose.connect(process.env.MOGO_URL as string, {})
+.then((data) => {
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3003;
+})
+.catch((err) => console.log("ERROR on connection MongoDB", err));
