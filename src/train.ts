cd5c02b8f9f550f66b7d4ac259d32c40f1a07708
Sam <kimjeyeon03@icloud.com>
2024-06-06 16:22:34 +0900

    feat: train file H -TASK

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,18 @@
-console.log("Train Area!");
+// H - TASK
+
+let a: number[];
+
+function plus(a: number[]) {
+    const musbat: number[] = [];
+    for (let i = 0; i <=a.length; i++) {
+        if(a[i] > 0) {
+            musbat.push(a[i]);
+        }
+    }
+    const b =  musbat.toString();
+    // const b =  musbat.join("");
+    return b;
+}
+
+console.log(plus([1, -7, 2, 3, 4, -9, 5, 6, -1, -5]));
+
