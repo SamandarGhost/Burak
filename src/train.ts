624fa4f57dfe3d27f3798bca3f898b9e799c0afb
Sam <kimjeyeon03@icloud.com>
2024-06-15 21:34:29 +0900

    fix: train page

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,24 @@
+function engKopi(a: number[]): number | undefined {
+    const b: { [key: number]: number } = {};
+  
+    for (const num of a) {
+      b[num] = (b[num] || 0) + 1;
+    }
+  
+    let c = 0;
+    let eng: number | undefined;
+  
+    for (const key in b) {
+      if (b[key] > c) {
+        c = b[key];
+        eng = Number(key);
+      }
+    }
+  
+    return eng;
+}
+console.log(engKopi([1, 2, 3, 0, 4, 5, 0, 6, 0, 7, 8, 9, 9, 9, 9, 9]));
+
 /*  Project Standards
         - Logging standards
         - Naming standards
