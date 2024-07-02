947365206fe537b994cda3c232e7519a230007e1
Sam <kimjeyeon03@icloud.com>
2024-07-02 19:48:18 +0900

    feat: create new product frontend logic

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -40,12 +40,12 @@ productController.createNewProduct = async (req: AdminRequest, res: Response) =>
 
         await productService.createNewProduct(data);
 
-        res.send(`<script> alert("Sucessful creation!"); window.location.replace("/admin/product/all) </script>`);
+        res.send(`<script> alert("Sucessful creation!"); window.location.replace("/admin/product/all") </script>`);
     } catch (err) {
         console.log("Error, createNewProduct:", err);
         const message = 
         err instanceof Errors ? err.message: Message.SOMETHING_WENT_WRONG;
-        res.send(`<script> alert("${message}"); window.location.replace("/admin/product/all) </script>`);
+        res.send(`<script> alert("${message}"); window.location.replace("/admin/product/all") </script>`);
     }
 };
 
