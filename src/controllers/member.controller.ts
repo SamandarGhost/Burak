e7cdef237c87c8414f9ef60cc9b396c8a5b99f1a
Sam <kimjeyeon03@icloud.com>
2024-07-21 14:32:18 +0900

    feat: develop authentication via tokens vs cookies

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -2,8 +2,9 @@ import {Request, Response} from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
-import Errors from "../libs/Errors";
+import Errors, { HttpCode } from "../libs/Errors";
 import AuthService from "../models/Auth.service";
+import { AUTH_TIMER } from "../libs/config";
 
 // React Project
 
@@ -17,11 +18,12 @@ memberController.signup = async (req: Request, res: Response) => {
         const input: MemberInput = req.body,
           result: Member = await memberService.signup(input);
           const token = await authService.createToken(result);
-          console.log("token 2 => ", token);
-          
-        //   TODO: TOKEN AUTHENTICATION
 
-        res.json({ member: result });
+          res.cookie("acesstoken", token, {maxAge: AUTH_TIMER * 3600 *1000,
+            httpOnly: false,
+         });              
+
+        res.status(HttpCode.CREATED).json({ member: result, accessToken: token });
     } catch (err) {
         if(err instanceof Errors) res.status(err.code).json(err);
         else res.status(Errors.standard.code).json(Errors.standard);
@@ -36,12 +38,12 @@ memberController.signup = async (req: Request, res: Response) => {
             const input: LoginInput = req.body,
               result = await memberService.login(input),
               token = await authService.createToken(result);
-              console.log("token => ", token);
               
-            //   TODO: TOKEN AUTHENTICATION
-            
+            res.cookie("acesstoken", token, {maxAge: AUTH_TIMER * 3600 *1000,
+                httpOnly: false,
+             });              
 
-            res.json({ member: result });
+            res.status(HttpCode.OK).json({ member: result, accessToken: token });
         } catch (err) {
             console.log("Error, login:", err);
             if(err instanceof Errors) res.status(err.code).json(err);
